93551ae7391124258d43951a9299e44313e0d067
yash hingu <yashhingu240303@gmail.com>
2024-01-30 15:35:52 -0700

    Solved error

---
+++ app/week-2/page.js
@@ -0,0 +1,9 @@
+import StudentInfo from './studentinfo';
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
---
+++ app/week-2/studentinfo.js
@@ -0,0 +1,14 @@
+
+import Link from "next/link";
+export default function StudentInfo() {
+
+  return (
+    <div>
+      <p>My Name: Yash Hingu</p>
+      <p>
+        <Link href="https://github.com/yashhingu2403/cprg306-assignments">My GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+};
